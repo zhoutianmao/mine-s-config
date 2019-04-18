39fd05d195f43b9bd2edd7ea899a44d97439398d
zhoutianmao <xczhenhk@163.com>
2019-04-19 00:15:10 +0800

    初始化

---
+++ src/main/java/com/ztm/config/BootApplication.java
@@ -0,0 +1,16 @@
+package com.ztm.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BootApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
